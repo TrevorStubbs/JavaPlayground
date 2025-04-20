c81e0980d9c453ee15d2dffa7fa2837baa264445
Trevor Stubbs <stubbste@gmail.com>
2025-04-20 16:06:51 -0700

    test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,6 +15,7 @@ public class Main {
     public static void main(String[] args) {
         var thing = DailyPracticeApril20.printClosest(new int[] {1,2,3,4}, new int[] {5, 6, 7, 8, 9}, 7);
 
+        // test commit
         System.out.println(Arrays.toString(thing));
     }
 }
